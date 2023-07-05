15dd815d89d2778ef7e5e44b81db81740deae5bb
harycoder1 <harish7823@gmail.com>
2023-07-05 19:41:27 +0530

    this commit for saved the all auth working successfully

--- utils/database.js
+++ utils/database.js
@@ -1,26 +1,26 @@
-import  mongoose from 'mongoose'
+import mongoose from 'mongoose';
 
-let isConnected = false ; //track the connection
+let isConnected = false; // track the connection
 
-export const connectToDB =async()=>{
-    mongoose.set('strictQuery',true)
+export const connectToDB = async () => {
+  mongoose.set('strictQuery', true);
 
-    if(isConnected){
-        console.log("MongoDB is already Connected!")
-        return ; //for stop the function
-    }
-// for established the connection
-try{
-await mongoose.connect(process.env.MONGODB_URI,{
-    dbName:"share_prompt",
-    useNewUrlParser:true ,
-    useUrlfieldTopology:true ,
-});
+  if(isConnected) {
+    console.log('MongoDB is already connected');
+    return;
+  }
 
-isConnected =true ;
-console.log("MongoDB connected")
+  try {
+    await mongoose.connect(process.env.MONGODB_URI, {
+      dbName: "share_prompt",
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
 
-}catch(error){}
-console.log(error)
+    isConnected = true;
+
+    console.log('MongoDB connected')
+  } catch (error) {
+    console.log(error);
+  }
 }
- 
